8ac7d1c3a1d6e11e8aab444dc54e6e4b882245c8
Vadim Harenco <harenco@yahoo.com>
2024-01-29 12:41:11 -0800

    add LocalStorage class and tests

--- src/data_polyglot/__init__.py
+++ src/data_polyglot/__init__.py
@@ -2,4 +2,4 @@ __version__ = "1.1.1"
 from .serializable import Serializable
 from .serializable import serialize
 from .table_data import TableData
-
+from .storage.local_storage import LocalStorage
